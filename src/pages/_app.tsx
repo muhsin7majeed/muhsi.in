013541fb741ef7370bba5ee3db1ce2cb2c9b8286
muhsin <muhsin7ak@gmail.com>
2023-04-11 16:32:50 +0530

    INIT: Setup Chakra, Base Nav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import SidebarWithHeader from "@/components/navbar/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    semanticTokens: {
+      colors: {},
+    },
+  });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <SidebarWithHeader>
+        <Component {...pageProps} />
+      </SidebarWithHeader>
+    </ChakraProvider>
+  );
 }
