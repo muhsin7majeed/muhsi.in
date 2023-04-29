92a5aad6f33a71cc2cfb6c78e1da8de07cc64e1f
muhsin <muhsin7ak@gmail.com>
2023-04-29 15:10:43 +0530

    FEATURE: Subskills popup

--- src/__DATA__/skills.ts
+++ src/__DATA__/skills.ts
@@ -1,63 +1,76 @@
 import { DiReact, DiAngularSimple, DiJavascript1, DiHtml5, DiCss3, DiGitBranch } from "react-icons/di";
-import { SiTypescript, SiFigma } from "react-icons/si";
+import { SiTypescript, SiFigma, SiNextdotjs, SiRedux, SiChakraui, SiStyledcomponents, SiFramer, SiReactquery } from "react-icons/si";
+import { BsBootstrap } from "react-icons/bs";
+import { AiOutlineForm } from "react-icons/ai";
+import { TbBrandStorybook } from "react-icons/tb";
 
-const SKILLS_LIST = [
-  [
-    {
-      name: "ReactJs / NextJs",
-      color: "#61dbfb",
-      icon: DiReact,
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling, Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
-    {
-      name: "JavaScript",
-      icon: DiJavascript1,
-      color: "#F0DB4F",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
-    {
-      name: "TypeScript",
-      icon: SiTypescript,
-      color: "#007acc",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
-  ],
-  [
-    {
-      name: "Angular 2+",
-      icon: DiAngularSimple,
-      color: "#dd1b16 ",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
+import { Skill } from "@/types/Skills";
 
-    {
-      name: "CSS3",
-      icon: DiCss3,
-      color: "#264de4",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling, Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
-    {
-      name: "HTML5",
-      icon: DiHtml5,
-      color: "#e34c26",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
-  ],
-  [
-    {
-      name: "GIT",
-      icon: DiGitBranch,
-      color: "#333",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling, Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
+const SKILLS_LIST: Skill[] = [
+  {
+    name: "ReactJs",
+    color: "#61dbfb",
+    icon: DiReact,
+    // TODO: Fix layout when too many subskills
+    subSkills: [
+      { name: "Redux", color: "#764abc", icon: SiRedux },
+      { name: "ChakraUI", color: "teal", icon: SiChakraui },
+      { name: "Reactstrap", color: "#63d7c", icon: BsBootstrap },
+      { name: "Formik", color: "#004ec7", icon: AiOutlineForm },
+      { name: "Styled Components", color: "#ef69d3", icon: SiStyledcomponents },
+      { name: "Storybook", color: "#ff4785", icon: TbBrandStorybook },
+      { name: "Framer Motion", color: "#000000", icon: SiFramer },
+      { name: "Reaqwect Query", color: "#f15d35", icon: SiReactquery },
+      { name: "React s", color: "#f15d35", icon: SiReactquery },
+      { name: "g Query", color: "#f15d35", icon: SiReactquery },
+    ],
+  },
+  {
+    name: "NextJs",
+    color: "#333",
+    icon: SiNextdotjs,
+  },
+  {
+    name: "JavaScript",
+    icon: DiJavascript1,
+    color: "#F0DB4F",
+    subSkills: [{ name: "Redux", color: "#333", icon: SiRedux }],
+  },
+  {
+    name: "TypeScript",
+    icon: SiTypescript,
+    color: "#007acc",
+  },
 
-    {
-      name: "Figma",
-      icon: SiFigma,
-      color: "#e34c26",
-      desc: "Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling, Ive built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-    },
-  ],
+  {
+    name: "Angular 2+",
+    icon: DiAngularSimple,
+    color: "#dd1b16 ",
+    subSkills: [{ name: "Redux", color: "#333", icon: SiRedux }],
+  },
+
+  {
+    name: "CSS3",
+    icon: DiCss3,
+    color: "#264de4",
+  },
+  {
+    name: "HTML5",
+    icon: DiHtml5,
+    color: "#e34c26",
+  },
+
+  {
+    name: "GIT",
+    icon: DiGitBranch,
+    color: "#333",
+  },
+
+  {
+    name: "Figma",
+    icon: SiFigma,
+    color: "#e34c26",
+  },
 ];
 
 export default SKILLS_LIST;
--- src/features/landing/SkillsSection.tsx
+++ src/features/landing/SkillsSection.tsx
@@ -1,37 +1,117 @@
-import { Container, Flex, Heading, Box, Text, Card, Icon } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Heading,
+  Box,
+  Text,
+  Card,
+  Icon,
+  Grid,
+  GridItem,
+  IconButton,
+  Button,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { FcInfo } from "react-icons/fc";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 import SKILLS_LIST from "@/__DATA__/skills";
 import SkillsIcon from "@/components/svgs/SkillsIcon";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import SkillCard from "@/components/SkillCard";
+import { Skill } from "@/types/Skills";
 
 const SkillsSection = () => {
+  const [selectedSkill, setSelectedSkill] = useState<Skill | null>(null);
+
+  const clearSelection = () => {
+    setSelectedSkill(null);
+  };
+
   return (
     <Container maxW={"7xl"} className="skills__section" py={24}>
       <Flex alignItems="center" gap={8} direction={["column-reverse", "column-reverse", "row"]} h="90%">
-        <Flex direction="column" alignItems="start" justifyContent="center" w="100%" h="100%">
-          <Heading fontSize={"4xl"} mb={4}>
-            What do I know?
-          </Heading>
+        <Flex
+          direction="column"
+          alignItems={["center", "center", "start"]}
+          justifyContent="center"
+          w="100%"
+          h="100%"
+          pos="relative"
+        >
+          <Heading fontSize={"4xl"}>What do I know?</Heading>
 
-          <Flex gap={3} direction={["column", "row", "row"]}>
-            {SKILLS_LIST.map((column, index) => (
-              <Box key={index} width={"100%"}>
-                {column.map((skill, index) => (
-                  <Card key={index} p={2} mb={4} boxShadow="lg">
-                    <Flex alignItems="center" gap={2}>
-                      <Icon fontSize={45} as={skill.icon} p={1} color={skill.color} />
+          <Text mt={4} mb={8} color="gray.500" textAlign={["center", "center", "start"]}>
+            Hint: Click on a card with a <Icon as={FcInfo} verticalAlign="middle" /> icon to learn more about related
+            skills.
+          </Text>
 
-                      <Heading fontSize={22}>{skill.name}</Heading>
-                    </Flex>
+          <Grid gap={3} templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]}>
+            <AnimatePresence>
+              {selectedSkill && (
+                <Box
+                  as={motion.div}
+                  layoutId={selectedSkill.name}
+                  pos="absolute"
+                  w="100%"
+                  h="100%"
+                  top={0}
+                  left={0}
+                  zIndex={1}
+                  backdropFilter="auto"
+                  backdropBlur="16px"
+                  boxShadow="lg"
+                >
+                  <Flex alignItems="center" justifyContent="space-between" mb={4} bgColor="white" p={4}>
+                    <Heading fontSize={"2xl"} color="blackAlpha.700">
+                      What do I know about{" "}
+                      <Text as="span" color={selectedSkill.color}>
+                        {selectedSkill.name}
+                      </Text>
+                      ?
+                    </Heading>
 
-                    <Text px={2}>{skill.desc}</Text>
-                  </Card>
-                ))}
-              </Box>
+                    <IconButton
+                      aria-label="hide more info"
+                      icon={<IoClose />}
+                      fontSize={16}
+                      onClick={clearSelection}
+                      variant="ghost"
+                      colorScheme="primary"
+                    />
+                  </Flex>
+
+                  <Grid
+                    gap={3}
+                    templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]}
+                    pos="relative"
+                    p={4}
+                  >
+                    {selectedSkill.subSkills?.map((skill) => {
+                      return (
+                        <GridItem key={skill.name}>
+                          <SkillCard skill={skill} />
+                        </GridItem>
+                      );
+                    })}
+                  </Grid>
+                </Box>
+              )}
+            </AnimatePresence>
+
+            {SKILLS_LIST.map((skill) => (
+              <GridItem key={skill.name}>
+                <motion.div layoutId={skill.name}>
+                  <SkillCard skill={skill} handleSkillSelection={setSelectedSkill} />
+                </motion.div>
+              </GridItem>
             ))}
-          </Flex>
+          </Grid>
         </Flex>
 
-        <Box w="50%">
+        <Box w="100%">
           <SkillsIcon />
         </Box>
       </Flex>
