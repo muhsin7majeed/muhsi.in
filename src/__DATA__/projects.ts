381f21f901a477628aea7afb1ae04417f5d9bd33
muhsin7majeed <muhsin7ak@gmail.com>
2025-11-07 12:09:52 +0000

    new: project

--- src/__DATA__/projects.ts
+++ src/__DATA__/projects.ts
@@ -1,4 +1,5 @@
 import { SiReact, SiTypescript, SiPixiv } from "react-icons/si";
+import { DiJavascript1, DiHtml5, DiCss3 } from "react-icons/di";
 
 import { Project } from "@/types/Project";
 
@@ -15,6 +16,20 @@ const PROJECTS_DATA: Project[] = [
       { name: "React", icon: SiReact, color: "#000000" },
       { name: "TypeScript", icon: SiTypescript, color: "#007acc" },
       { name: "PixiJS", icon: SiPixiv, color: "#319795" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Live Camera to ASCII Art",
+    description:
+      "A real-time webcam to ASCII art converter built with vanilla JavaScript, HTML5 Canvas, and MediaDevices API.",
+    image: "/assets/live-cam-ascii.png",
+    githubUrl: "https://github.com/muhsin7majeed/live-cam-ascii",
+    websiteUrl: "https://live-ascii.muhsi.in/",
+    technologies: [
+      { name: "JavaScript", icon: DiJavascript1, color: "#F0DB4F" },
+      { name: "HTML5", icon: DiHtml5, color: "#e34c26" },
+      { name: "CSS3", icon: DiCss3, color: "#264de4" },
     ],
   },
 ];
